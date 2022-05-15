b6cb6f623dd28cf9183f773bb3788a699d337def
zs <2108995673@qq.com>
2022-05-15 14:00:48 +0800

    关于mp 的插件的配置类  乐观锁，分页插件

---
+++ mybatis_plus/src/main/java/com/xff/config/MpConfig.java
@@ -0,0 +1,29 @@
+package com.xff.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration   //表示是配置类
+@MapperScan("com.xff.mapper")  //spring boot扫描
+public class MpConfig {
+    /**
+     * 乐观锁插件
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+
+}
