75df3fc80c375083191b41713758b8ee136867b3
zs <2108995673@qq.com>
2022-05-15 14:03:14 +0800

    自动填充的过滤器，拦截执行

---
+++ mybatis_plus/src/main/java/com/xff/hander/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.xff.hander;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component  //交给spring扫描
+public class MyMetaObjectHandler implements MetaObjectHandler {
+                           // 实现这个接口 重写方法，
+    //mp执行添加操作，这个方法执行
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime",new Date(),metaObject);
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+        this.setFieldValByName("version",1,metaObject); //添加时的版本号
+        this.setFieldValByName("deleted",0,metaObject); // 填充逻辑删除的字段
+    }
+
+    //mp执行修改操作，这个方法执行
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+}
