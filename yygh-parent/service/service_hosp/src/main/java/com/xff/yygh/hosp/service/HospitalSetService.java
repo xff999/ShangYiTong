57a986b5c7e2d24b06a717dadcfbe3b8d78c6660
zs <2108995673@qq.com>
2022-05-17 19:03:12 +0800

    医院服务端service层（继承mp 的基本操作语句）

---
+++ yygh-parent/service/service_hosp/src/main/java/com/xff/yygh/hosp/service/HospitalSetService.java
@@ -0,0 +1,9 @@
+package com.xff.yygh.hosp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xff.yygh.model.hosp.HospitalSet;
+
+
+public interface HospitalSetService extends IService<HospitalSet> {
+    // service 继承 mp 提供的基础iservice ,传入实体类操作对象
+}
