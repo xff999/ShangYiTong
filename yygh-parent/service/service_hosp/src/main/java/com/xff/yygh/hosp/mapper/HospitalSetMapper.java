6e6edb25c14b50712849295d2e9fae9b8f25e586
zs <2108995673@qq.com>
2022-05-17 19:01:31 +0800

    医院服务端 Mapper映射文件

---
+++ yygh-parent/service/service_hosp/src/main/java/com/xff/yygh/hosp/mapper/HospitalSetMapper.java
@@ -0,0 +1,8 @@
+package com.xff.yygh.hosp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xff.yygh.model.hosp.HospitalSet;
+
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+    //基础的医院的增删改查mapper映射语句  继承mp 的 传入实体类 (要在pom 中引入model 模块）
+}
