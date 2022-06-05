be869807f4267d2e39b77f9cb230d3b922b77c4a
zs <2108995673@qq.com>
2022-06-05 12:17:29 +0800

    医院模拟系统的模块

---
+++ yygh-parent/hospital-manage/src/main/java/com/xff/HospitalManageApplication.java
@@ -0,0 +1,13 @@
+package com.xff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManageApplication {
+      //localhost:9998 访问
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManageApplication.class, args);
+    }
+
+}
---
+++ yygh-parent/hospital-manage/src/main/java/com/xff/hospital/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.xff.hospital.mapper;
+
+import com.xff.hospital.model.Schedule;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
