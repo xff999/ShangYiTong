05d38b32c5120d4e6b9d7e5ae117db2754752cc6
zs <2108995673@qq.com>
2022-06-05 12:13:17 +0800

    jwt字符串 cookie 的工具类

---
+++ yygh-parent/common/common_util/src/main/java/com/xff/yygh/common/help/JwtHelper.java
@@ -0,0 +1,47 @@
+package com.xff.yygh.common.help;
+
+
+import io.jsonwebtoken.*;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+public class JwtHelper {
+    private static long tokenExpiration = 24*60*60*1000;  //过期时间 ms
+    private static String tokenSignKey = "123456";  //签名 密钥
+
+    //根据参数 生成token
+    public static String createToken(Long userId, String userName) {
+        String token = Jwts.builder()
+                .setSubject("YYGH-USER")
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
+                .claim("userId", userId)
+                .claim("userName", userName)
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey) //签名hssh
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+        return token;
+    }
+    //根据token 字符串得到 用户信息 id
+    public static Long getUserId(String token) {
+        if(StringUtils.isEmpty(token)) return null;
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        Integer userId = (Integer)claims.get("userId");
+        return userId.longValue();
+    }
+    //根据token 字符串得到 用户信息 名称
+    public static String getUserName(String token) {
+        if(StringUtils.isEmpty(token)) return "";
+        Jws<Claims> claimsJws
+                = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        return (String)claims.get("userName");
+    }
+    public static void main(String[] args) {
+        String token = JwtHelper.createToken(1L, "55");
+        System.out.println(token);
+        System.out.println(JwtHelper.getUserId(token));
+        System.out.println(JwtHelper.getUserName(token));
+    }
+}
