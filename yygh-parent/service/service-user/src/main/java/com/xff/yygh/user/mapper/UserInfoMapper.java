a2ac5f47fc4c20f1d643599d6303e583d693dfe8
zs <2108995673@qq.com>
2022-06-08 19:13:17 +0800

    用户操作

---
+++ yygh-parent/service/service-user/src/main/java/com/xff/yygh/user/mapper/UserInfoMapper.java
@@ -0,0 +1,7 @@
+package com.xff.yygh.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xff.yygh.model.user.UserInfo;
+
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+}
