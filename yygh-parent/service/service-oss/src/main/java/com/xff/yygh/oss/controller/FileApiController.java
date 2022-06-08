d5d262327176cd56b0e1a658aac5bf0fb0d7cc5b
zs <2108995673@qq.com>
2022-06-08 19:09:31 +0800

    阿里对象上传存储模块

---
+++ yygh-parent/service/service-oss/src/main/java/com/xff/yygh/oss/controller/FileApiController.java
@@ -0,0 +1,27 @@
+package com.xff.yygh.oss.controller;
+
+
+import com.xff.yygh.common.result.Result;
+import com.xff.yygh.oss.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/oss/file")
+public class FileApiController {
+    @Autowired
+    private FileService fileService;
+
+//上传文件到阿里云oss
+    @PostMapping("fileUpload")
+    public Result fileUpload(MultipartFile file) {
+        //获取上传文件
+        String url = fileService.upload(file);
+
+        return Result.ok(url);
+    }
+
+}
---
+++ yygh-parent/service/service-oss/src/main/java/com/xff/yygh/oss/test/TestOss.java
@@ -0,0 +1,64 @@
+package com.xff.yygh.oss.test;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
+
+import java.io.ByteArrayInputStream;
+
+public class TestOss {
+    public static void main(String[] args) {
+
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
+// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tB5vDX7NGAtzxJPPyeU";
+        String accessKeySecret = "Q2PVs8YjjrkJUT6dh3q9rHtRGqSIwI";
+// 填写Bucket名称，例如examplebucket。
+        String bucketName = "syt-xff2";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+
+// 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "exampledir/exampleobject.txt";
+
+        // 创建OSSClient实例。
+
+        try {
+            // 填写字符串。
+            String content = "Hello OSS";
+
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
+
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            // ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+            // metadata.setObjectAcl(CannedAccessControlList.Private);
+            // putObjectRequest.setMetadata(metadata);
+
+            // 上传字符串。
+            ossClient.putObject(putObjectRequest);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+}
