2a0f6644d83106020212e5649062cbc257cab72a
zs <2108995673@qq.com>
2022-06-08 19:08:00 +0800

    医院信息的远程调用模块

---
+++ yygh-parent/service-client/service-hosp-client/src/main/java/com/xff/yygh/hosp/client/HosptialFeignClient.java
@@ -0,0 +1,26 @@
+package com.xff.yygh.hosp.client;
+
+
+import com.xff.yygh.vo.hosp.ScheduleOrderVo;
+import com.xff.yygh.vo.order.SignInfoVo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Repository
+@FeignClient("service-hosp")
+public interface HosptialFeignClient {
+
+    /**
+     * 根据排班id获取预约下单数据
+     */
+    @GetMapping("/api/hosp/hospital/inner/getScheduleOrderVo/{scheduleId}")
+    ScheduleOrderVo getScheduleOrderVo(@PathVariable("scheduleId") String scheduleId);
+    /**
+     * 获取医院签名信息
+     */
+    @GetMapping("/api/hosp/hospital/inner/getSignInfoVo/{hoscode}")
+    SignInfoVo getSignInfoVo(@PathVariable("hoscode") String hoscode);
+
+}
